57bda735088b5baff85b39c5f835dd1963078338
Jorge Moratinos Salcines <jorge.moratinossalcines@telefonica.com>
2025-02-18 16:52:30 +0100

    Included ingress nginx

--- community-apps/appliances/OCF/appliance.sh
+++ community-apps/appliances/OCF/appliance.sh
@@ -27,11 +27,17 @@ VARIABLES_FILE="${BASE_DIR}/services/variables.sh"
 DOCKER_ROBOT_IMAGE="labs.etsi.org:5050/ocf/capif/robot-tests-image"
 DOCKER_ROBOT_IMAGE_VERSION=1.0
 OCF_ROBOT_FRAMEWORK_VERSION="${DOCKER_ROBOT_IMAGE_VERSION}-amd64"
+DOCKER_COMPOSE_CAPIF_FILE="${BASE_DIR}/services/docker-compose-capif.yml"
+INGRESS_NGINX_VERSION=1.27.4
+INGRESS_NGINX_DIR="${BASE_DIR}/ingress-nginx"
+DOCKER_COMPOSE_INGRESS_NGINX_FILE="${INGRESS_NGINX_DIR}/docker-compose-ingress-nginx.yml"
+DOCKER_COMPOSE_INGRESS_NGINX_CONF_FILE="${INGRESS_NGINX_DIR}/nginx.conf"
 
 # Configurable variables only for config and bootstrap, set by default on the VM Template
 # ONEAPP_OCF_USER="${ONEAPP_OCF_USER:-'client'}"
 # ONEAPP_OCF_PASSWORD="${ONEAPP_OCF_PASSWORD:-'password'}"
 # CAPIF_HOSTNAME="${CAPIF_HOSTNAME:-'capifcore'}"
+# REGISTER_HOSTNAME="${REGISTER_HOSTNAME:-'register'}"
 
 
 # ------------------------------------------------------------------------------
@@ -55,12 +61,6 @@ service_install()
     # install yq
     install_yq
 
-    # nginx
-    # install_nginx
-
-    # create local folders to run capif services.
-    # create_local_folders
-
     # Download locally the docker images
     download_images
 
@@ -80,6 +80,9 @@ service_configure()
 {
     export DEBIAN_FRONTEND=noninteractive
 
+    # Configure Ingress Nginx
+    configure_ingress_nginx
+
     # Setup environment variables for deployment
     setup_environment
 
@@ -91,10 +94,14 @@ service_bootstrap()
     export DEBIAN_FRONTEND=noninteractive
 
     msg info "Starting OpenCAPIF Services"
-    run_docker_compose
+    run_open_capif
 
+    msg info "Starting Ingress Nginx"
+    run_ingress_nginx
+
+    msg info "Waiting for services to start"
     sleep 2m
-
+    
     msg info "Creating OpenCAPIF user"
     create_user
 
@@ -151,34 +158,13 @@ install_docker()
     fi
 }
 
-# create_local_folders()
-# {
-#     msg info "Create Local Folders for each service"
-#     services=(
-#         "TS29222_CAPIF_API_Invoker_Management_API"
-#         "TS29222_CAPIF_API_Provider_Management_API"
-#         "TS29222_CAPIF_Access_Control_Policy_API"
-#         "TS29222_CAPIF_Auditing_API"
-#         "TS29222_CAPIF_Discover_Service_API"
-#         "TS29222_CAPIF_Events_API"
-#         "TS29222_CAPIF_Logging_API_Invocation_API"
-#         "TS29222_CAPIF_Publish_Service_API"
-#         "TS29222_CAPIF_Routing_Info_API"
-#         "TS29222_CAPIF_Security_API"
-#         "helper"
-#         "mock_server"
-#         "monitoring"
-#         "nginx"
-#         "redis-data"
-#         "redis.conf"
-#         "register"
-#         "vault"
-#     )
-#     for service in "${services[@]}"; do
-#         msg info "Create Local Folders for $service"
-#         mkdir -p /etc/one-appliance/service.d/$service
-#     done
-# }
+install_yq()
+{
+    msg info "Install yq"
+    add-apt-repository ppa:rmescandon/yq
+    apt update
+    apt install yq -y
+}
 
 download_images()
 {
@@ -203,69 +189,13 @@ download_images()
         mongo-express:1.0.0-alpha.4
         redis:alpine
         ${DOCKER_ROBOT_IMAGE}:${OCF_ROBOT_FRAMEWORK_VERSION}
+        nginx:${INGRESS_NGINX_VERSION}
     )
 
     for image in "${images[@]}"; do
         msg info "Pull image $image from etsi repository"
         docker pull $image
     done
-}
-
-
-#under development
-# install_nginx()
-# {
-#     apt update
-#     msg info "Install Nginx"
-#     if ! apt-get install -y nginx ; then
-#         msg error "Nginx installation failed"
-#         exit 1
-#     fi
-#     mkdir -p /etc/nginx/certs
-#     openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-#         -keyout /etc/nginx/certs/server.key -out /etc/nginx/certs/server.crt \
-#         -subj "/CN=localhost"
-#     cat > /etc/nginx/sites-available/default <<EOF
-# events {}
-
-# http {
-#     map \$host \$backend {
-#         default "127.0.0.1:8080";  # Por defecto, redirige a 8080
-#         register-opencapif "127.0.0.1:8084";
-#         opencapif "127.0.0.1:8080";
-#     }
-
-#     server {
-#         listen 443 ssl;
-#         server_name register-opencapif opencapif;
-
-#         ssl_certificate /etc/nginx/certs/server.crt;
-#         ssl_certificate_key /etc/nginx/certs/server.key;
-
-#         location / {
-#             proxy_pass http://\$backend;
-#             proxy_set_header Host \$host;
-#             proxy_set_header X-Real-IP \$remote_addr;
-#             proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-#             proxy_set_header X-Forwarded-Proto https;
-#         }
-#     }
-# }
-# EOF
-#     sudo nginx -t
-#     cat /etc/nginx/sites-available/default
-#     sudo systemctl restart nginx
-#     sudo systemctl enable nginx
-#     sudo ufw allow 443/tcp
-#     sudo ufw reload
-# }
-
-postinstall_cleanup()
-{
-    msg info "Delete cache and stored packages"
-    apt-get autoclean
-    apt-get autoremove
-    rm -rf /var/lib/apt/lists/*
 }
 
 download_capif_repository()
@@ -275,17 +205,64 @@ download_capif_repository()
     git clone --branch OCFXX-Improve_local_scripts --single-branch https://labs.etsi.org/rep/ocf/capif.git ${BASE_DIR}
 }
 
-run_docker_compose()
+postinstall_cleanup()
 {
-    msg info "Run OpenCAPIF"
-    ${BASE_DIR}/services/clean_capif_docker_services.sh -a -z false
-    ${BASE_DIR}/services/run.sh -s
+    msg info "Delete cache and stored packages"
+    apt-get autoclean
+    apt-get autoremove
+    rm -rf /var/lib/apt/lists/*
 }
 
-create_user()
+configure_ingress_nginx()
 {
-    msg info "Create OpenCAPIF user"
-    ${BASE_DIR}/services/create_users.sh -u ${ONEAPP_OCF_USER} -p ${ONEAPP_OCF_PASSWORD} -t 1
+    msg info "Deploy Ingress Nginx"
+    mkdir $INGRESS_NGINX_DIR
+    # Create nginx configuration file
+    cat > $DOCKER_COMPOSE_INGRESS_NGINX_CONF_FILE <<EOF
+worker_processes auto;
+error_log /var/log/nginx/error.log debug;  # Modo debug
+
+events {
+    worker_connections 1024;
+}
+
+stream {
+    log_format basic '\$remote_addr [\$time_local] '
+                     '"\$protocol" "\$ssl_preread_server_name" '
+                     '=> "\$backend"';
+
+    access_log /var/log/nginx/access.log basic;
+
+    map \$ssl_preread_server_name \$backend {
+        ${REGISTER_HOSTNAME} 127.0.0.1:8084;
+        ${CAPIF_HOSTNAME} 127.0.0.1:8443;
+        default 127.0.0.1:8080;  # En caso de que el hostname no coincida
+    }
+
+    server {
+        listen 443;
+        proxy_pass \$backend;
+        ssl_preread on;
+    }
+}
+EOF
+    # Create nginx docker compose file
+    msg info "Create docker-compose file for Ingress Nginx"
+    cat > $DOCKER_COMPOSE_INGRESS_NGINX_FILE <<EOF
+version: '3.8'
+
+services:
+  nginx:
+    image: nginx:${INGRESS_NGINX_VERSION}
+    container_name: nginx_proxy
+    ports:
+      - "443:443"  # port 443 of host exposed
+    volumes:
+      - $DOCKER_COMPOSE_INGRESS_NGINX_CONF_FILE:/etc/nginx/nginx.conf:ro  # nginx configuration
+    network_mode: "host"  # use host network
+    restart: unless-stopped
+EOF
+
 }
 
 setup_environment()
@@ -294,16 +271,39 @@ setup_environment()
     sed -i "s|^export REGISTRY_BASE_URL=.*|export REGISTRY_BASE_URL=\"$REGISTRY_BASE_URL\"|" "$VARIABLES_FILE"
     sed -i "s|^export OCF_VERSION=.*|export OCF_VERSION=\"$OCF_VERSION\"|" "$VARIABLES_FILE"
     sed -i "s|^export CAPIF_HOSTNAME=.*|export CAPIF_HOSTNAME=\"$CAPIF_HOSTNAME\"|" "$VARIABLES_FILE"
+    sed -i "s|^export CAPIF_HTTPS_PORT=.*|export CAPIF_HTTPS_PORT=\"443\"|" "$VARIABLES_FILE"
+    sed -i "s|^export CAPIF_REGISTER=.*|export CAPIF_REGISTER=\"$REGISTER_HOSTNAME\"|" "$VARIABLES_FILE"
+    sed -i "s|^export CAPIF_REGISTER_PORT=.*|export CAPIF_REGISTER_PORT=\"443\"|" "$VARIABLES_FILE"
     sed -i "s|^export BUILD_DOCKER_IMAGES=.*|export BUILD_DOCKER_IMAGES=false|" "$VARIABLES_FILE"
     sed -i "s|^export DOCKER_ROBOT_IMAGE_VERSION=.*|export DOCKER_ROBOT_IMAGE_VERSION=$DOCKER_ROBOT_IMAGE_VERSION|" "$VARIABLES_FILE"
     sed -i "s|^export DOCKER_ROBOT_IMAGE=.*|export DOCKER_ROBOT_IMAGE=$DOCKER_ROBOT_IMAGE|" "$VARIABLES_FILE"
     sed -i "s|^export DOCKER_ROBOT_TTY_OPTIONS=.*|export DOCKER_ROBOT_TTY_OPTIONS="-i"|" "$VARIABLES_FILE"
+
+    # Edit docker-compose-capif to expose nginx on port 8443 and leave 443 for ingress nginx
+    msg info "Expose OpenCAPIF services on port 8080 and 8443"
+    yq eval ".services.nginx.ports[0] = \"8080:8080\"" -i "$DOCKER_COMPOSE_CAPIF_FILE"
+    yq eval ".services.nginx.ports[1] = \"8443:443\"" -i "$DOCKER_COMPOSE_CAPIF_FILE"
+
 }
 
-install_yq()
+run_open_capif()
 {
-    msg info "Install yq"
-    add-apt-repository ppa:rmescandon/yq
-    apt update
-    apt install yq -y
+    msg info "Run OpenCAPIF"
+    ${BASE_DIR}/services/clean_capif_docker_services.sh -a -z false
+    ${BASE_DIR}/services/run.sh -s
 }
+
+run_ingress_nginx(){
+    msg info "Run Ingress Nginx"
+    docker compose -f ${DOCKER_COMPOSE_INGRESS_NGINX_FILE} up -d
+}
+
+create_user()
+{
+    msg info "Create OpenCAPIF user"
+    ${BASE_DIR}/services/create_users.sh -u ${ONEAPP_OCF_USER} -p ${ONEAPP_OCF_PASSWORD} -t 1
+}
+
+
+
+
